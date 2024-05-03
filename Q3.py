ce2b2f78a830464c68afa09cc990956a1bb75c28
TzarNikolai <104406569+TzarNikolai@users.noreply.github.com>
2024-05-03 13:05:01 +1000

    Update Q3.py
    updated code.
    TypeError: 'Float' object is not iterable

--- Q3.py
+++ Q3.py
@@ -1,24 +1,22 @@
-def calc_mean(sline):
-   sum_of_list = 0 
-
-   for i in range(len(sline)):
-      sum_of_list += sline[i]
-   average = sum_of_list/len(sline)        
-   return average
-
-
+def calc_mean(numbers_array):
+    sum_of_list = 0 
+    for i in range(len(numbers_array)):
+        for j in range(len(numbers_array[i])):
+            sum_of_list += int(numbers_array[i][j])
+            average = sum_of_list/len(numbers_array[i][j])        
+        return average
+    
 filename = input("File name: ")
 with open(filename, 'r') as file:
-   lines = file.readlines()
-   first_line = lines[:1]
+    lines = file.readlines()
+    numbers_array = []
+    for line in lines:
+        numbers_array.append(line.split())
 
-   for line in first_line:
-         sline = line.strip()
-         sline = sline.split()
-      
-   calc_mean(sline)
-   mean_of_num = calc_mean(sline)
-   print(mean_of_num)
+
+print_mean = calc_mean(numbers_array)
+for i, avg in enumerate(print_mean):
+    print(f"Average of line {i+1}:{avg}")
 
    
       
